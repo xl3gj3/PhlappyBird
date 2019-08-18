fecb76c6c93dfbeeffa1e4c2db9ad923f074eb70
daphne0029 <daphne0029@gmail.com>
2019-08-18 10:58:42 -0700

    convert tabs to 4 spaces

--- src/Processor.js
+++ src/Processor.js
@@ -1,13 +1,13 @@
 var ProcessorFactory = function(model) {
-	const localData = {};
-	const processor = {};
+    const localData = {};
+    const processor = {};
 
-	processor.process = function() {
-		// calculate all the physics logic here
-		// - calculate velocity
-		// - detect unit collision
-		// - read user control
-	}
+    processor.process = function() {
+        // calculate all the physics logic here
+        // - calculate velocity
+        // - detect unit collision
+        // - read user control
+    }
 
-	return processor;
+    return processor;
 }
--- src/Shape.js
+++ src/Shape.js
@@ -1,36 +1,36 @@
 var ObstacleFactory = function(wallOptions){
-	// shape.prototype.draw()
+    // shape.prototype.draw()
 
 }
 
 var BirdFactory = function(avatarOptions){
-	// shape.prototype.draw()
-	this.x = avatarOptions.x;
-	this.y = avatarOptions.y;
-	this.startTime = 0;
-	this.frameTick = 50; // ms
+    // shape.prototype.draw()
+    this.x = avatarOptions.x;
+    this.y = avatarOptions.y;
+    this.startTime = 0;
+    this.frameTick = 50; // ms
 };
 
 BirdFactory.prototype.draw = function(currentTime, ctx, canvas) {
-	ctx.save();
-	ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, false);
-	ctx.fillStyle = 'blue';
+    ctx.save();
+    ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, false);
+    ctx.fillStyle = 'blue';
     ctx.fill();
 
-    let frame = 1;
-    if (this.startTime === 0) {
-    	this.startTime = currentTime;
-    } else {
-    	frame = Math.floor( (currentTime - this.startTime ) / this.frameTick ) % 6;
-    }
+    // let frame = 1;
+    // if (this.startTime === 0) {
+    //     this.startTime = currentTime;
+    // } else {
+    //     frame = Math.floor( (currentTime - this.startTime ) / this.frameTick ) % 6;
+    // }
 
-    switch(frame) {
-    	case 1:
-    		this.drawFrame1(currentTime, ctx, canvas);
-    		break;
-    	default:
-    		break;
-    }
+    // switch(frame) {
+    //     case 1:
+    //         this.drawFrame1(currentTime, ctx, canvas);
+    //         break;
+    //     default:
+    //         break;
+    // }
 
-	ctx.restore();
+    ctx.restore();
 }
